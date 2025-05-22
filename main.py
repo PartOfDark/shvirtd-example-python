681d56d3082bbf5c49ecc794f919226aca858f18
elliot <roman.sigimov@ms11.ru>
2025-05-23 00:18:44 +0500

    added db_table

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-from flask import Flask
-from flask import request
 import os
-import mysql.connector
 from datetime import datetime
 
+import mysql.connector
+from flask import Flask, request
+
 app = Flask(__name__)
-db_host=os.environ.get('DB_HOST')
-db_user=os.environ.get('DB_USER')
-db_password=os.environ.get('DB_PASSWORD')
-db_database=os.environ.get('DB_NAME')
+db_host = os.environ.get("DB_HOST")
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD")
+db_database = os.environ.get("DB_NAME")
+db_table = os.environ.get("DB_TABLE")
 
 # Подключение к базе данных MySQL
 db = mysql.connector.connect(
-host=db_host,
-user=db_user,
-password=db_password,
-database=db_database,
-autocommit=True )
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_database,
+    autocommit=True,
+)
 cursor = db.cursor()
 
 # SQL-запрос для создания таблицы в БД
 create_table_query = f"""
-CREATE TABLE IF NOT EXISTS {db_database}.requests (
+CREATE TABLE IF NOT EXISTS {db_database}.{db_table} (
 id INT AUTO_INCREMENT PRIMARY KEY,
 request_date DATETIME,
 request_ip VARCHAR(255)
@@ -29,21 +31,22 @@ request_ip VARCHAR(255)
 """
 cursor.execute(create_table_query)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     # Получение IP-адреса пользователя
-    ip_address = request.headers.get('X-Forwarded-For')
+    ip_address = request.headers.get("X-Forwarded-For")
 
     # Запись в базу данных
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    query = "INSERT INTO requests (request_date, request_ip) VALUES (%s, %s)"
+    query = "INSERT INTO {db_table} (request_date, request_ip) VALUES (%s, %s)"
     values = (current_time, ip_address)
     cursor.execute(query, values)
     db.commit()
 
-    return f'TIME: {current_time}, IP: {ip_address}'
+    return f"TIME: {current_time}, IP: {ip_address}"
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
